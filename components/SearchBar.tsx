fc69cb6a723333523dd875540a3ac154a828337e
Kevin Kipkoech <mutaikevinkip.05@gmail.com>
2024-05-26 22:54:45 +0300

    create url validator

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,15 +1,46 @@
 "use client"
 
-import React from 'react'
+import { hostname } from 'os'
+import React, { FormEvent, useState } from 'react'
 
 const SearchBar = () => {
-    const handleSubmit = () => {}
+    const [searchPrompt, setSetSearchPrompt] = useState('')
+
+    const isValidAmazonProductURL = (url: string) => {
+      try {
+        const parsedURL = new URL(url)
+        const hostname = parsedURL.hostname 
+
+        if (
+          hostname.includes('amazon.com') ||
+          hostname.includes('amazon.') ||
+          hostname.endsWith('amazon')
+        ) {
+          return true
+        }        
+
+      } catch (error) {
+        console.log(error)
+        return false
+      }
+      return false
+    }
+
+    const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+      event.preventDefault()
+
+      const isValidLink = isValidAmazonProductURL(searchPrompt)
+
+    }
+
   return (
     <form className='flex flex-wrap gap-4 mt-12' onSubmit={handleSubmit}>
         <input 
             type='text'
             placeholder='Enter product link'
             className='searchbar-input'
+            value={searchPrompt}
+            onChange={(e) => setSetSearchPrompt(e.target.value)}
         />
         <button className='searchbar-btn' type='submit'>
             Search
