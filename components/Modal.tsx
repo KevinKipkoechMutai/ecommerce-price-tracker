cdee0f7ca9d745736a0cc2128831357c0177ae69
Kevin Kipkoech <mutaikevinkip.05@gmail.com>
2024-06-26 23:42:37 +0300

    create modal

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,14 +1,33 @@
 'use client'
 
-import React, { Fragment, useState } from 'react'
-import { Dialog, Transition, TransitionChild } from '@headlessui/react'
+import React, { FormEvent, Fragment, useState } from 'react'
+import { Dialog, Transition } from '@headlessui/react'
+import Image from 'next/image'
+import { addUserEmailToProduct } from '@/lib/actions'
 
-const Modal = () => {
+interface Props {
+    productId: string
+}
 
-    let [isOpen, setIsOpen] = useState(true)
+const Modal = ({ productId }: Props) => {
+
+    let [isOpen, setIsOpen] = useState(false)
+    const [isSubmitting, setIsSubmitting] = useState(false)
+    const [email, setEmail] = useState('')
 
     const openModal = () => setIsOpen(true)
     const closeModal = () => setIsOpen(false)
+
+    const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        setIsSubmitting(true)
+
+        await addUserEmailToProduct(productId, email)
+
+        setIsSubmitting(false)
+        setEmail('')
+        closeModal()
+    }
 
   return (
     <>
@@ -18,7 +37,7 @@ const Modal = () => {
         <Transition appear show={isOpen} as={Fragment}>
             <Dialog as='div' open={isOpen} onClose={closeModal} className='dialog-container'>
                 <div className='min-h-screen px-4 text-center'>
-                    <TransitionChild
+                    <Transition.Child
                         as={Fragment}
                         enterFrom='opacity-0'
                         enterTo='opacity100'
@@ -26,10 +45,12 @@ const Modal = () => {
                         leaveFrom='opacity-100'
                         leaveTo='opacity-0'
                     >
-                        <div>Child</div>
-                    </TransitionChild>
+                        <Dialog.Overlay className='fixed inset-0'/>
+                    </Transition.Child>
 
-                    <TransitionChild
+                    <span className='inline-block h-screen align-middle' aria-hidden='true'/>
+
+                    <Transition.Child
                         as={Fragment}
                         enter='ease-out duration-300'
                         enterFrom='opacity-0 scale-95'
@@ -38,8 +59,34 @@ const Modal = () => {
                         leaveFrom='opacity-100 scale-100'
                         leaveTo='opacity-0 scale-95'
                     >
-                        <div className='dialog-content'>test</div>
-                    </TransitionChild>
+                        <div className='dialog-content'>
+                            <div className='flex flex-col'>
+                                <div className='flex justify-between'>
+                                    <div className='p-3 border border-gray-200 rounded-10'>
+                                        <Image src='/assets/icons/logo.svg' alt='logo' width={28} height={28} />
+                                    </div>
+                                    <Image 
+                                        src='/assets/icons/x-close.svg'
+                                        alt='close'
+                                        width={24}
+                                        height={24}
+                                        className='cursor-pointer'
+                                        onClick={closeModal}
+                                    />
+                                </div>
+                                <h4 className='dialog-head_text'>Stay updated with product pricing alerts right in your inbox!</h4>
+                                <p className='text-sm text-gray-600 mt-2'>Never miss a bargain again with our timely alerts!</p>
+                            </div>
+                            <form className='flex flex-col mt-5' onSubmit={handleSubmit}>
+                                <label htmlFor='email' className='text-sm font-medium text-gray-700'>Email address</label>
+                                <div className='dialog-input_container'>
+                                    <Image src='/assets/icons/mail.svg' alt='email' height={18} width={18}/>
+                                    <input value={email} onChange={(e) => setEmail(e.target.value)} required type='email' id='email' placeholder='Enter your email address' className='dialog-input'/>
+                                </div>
+                                <button type='submit' className='dialog-btn'>{isSubmitting ? 'Submitting...' : 'Track'}</button>
+                            </form>
+                        </div>
+                    </Transition.Child>
 
                 </div>
             </Dialog>
